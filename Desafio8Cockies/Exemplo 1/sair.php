5fab5485c841bb9230dc1832515464f137cc3e14
Gabriel <bielaccadrollifarias@gmail.com>
2026-03-23 18:52:08 -0300

    Add files via upload

---
+++ Desafio8Cockies/Exemplo 1/sair.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+
+// Obs: o cookie só será apagado se o usuário não marcar "lembrar" no próximo login
+
+
+header('Location: index.php');
+exit;
+
+
